04c6406875f65b9029a8fdd50e48a2be22046171
Scott Brenner <brenner.scott.980@icloud.com>
2023-10-22 22:20:51 -0600

    Initial Commit

---
+++ Sources/SBFoundation/NumberFormatter+Extensions.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension NumberFormatter {
+    
+    /// Returns a string containing the formatted value of the provided integer.
+    /// - Author: Scott Brenner | SBFoundation
+    /// - Parameter integer: An `Int` object that is parsed to create the returned string object.
+    /// - Returns: A string containing the formatted value of the integer using the receiver’s current settings.
+    public func string(from integer: Int) -> String? {
+        string(from: NSNumber(integerLiteral: integer))
+    }
+}
