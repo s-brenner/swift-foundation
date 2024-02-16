0108118d02246e40b0f62e8d25e121fc513987ec
Scott Brenner <brenner.scott.980@icloud.com>
2024-02-16 13:13:22 -0700

    Updated packages and exports

--- Sources/SBFoundation/Internal/Exports.swift
+++ Sources/SBFoundation/Internal/Exports.swift
@@ -6,6 +6,10 @@
 @_exported import Tagged
 #endif
 
+#if canImport(SBFoundationMacros)
+@_exported import SBFoundationMacros
+#endif
+
 #if canImport(SBStandardLibrary)
 @_exported import SBStandardLibrary
 #endif
