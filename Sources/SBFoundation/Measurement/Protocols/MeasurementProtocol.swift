034413c2de793dfd45febd62eccf90cec24fc9ca
Scott Brenner <brenner.scott.980@icloud.com>
2024-10-21 14:46:28 -0600

    wip

--- Sources/SBFoundation/Measurement/Protocols/MeasurementProtocol.swift
+++ Sources/SBFoundation/Measurement/Protocols/MeasurementProtocol.swift
@@ -25,6 +25,35 @@ extension Measurement: MeasurementProtocol {
     }
 }
 
+extension UnkeyedDecodingContainer {
+    
+    /// Decodes a value of the given type.
+    /// - Parameter type: The type of value to decode.
+    /// - Parameter unit: The unit of measure.
+    /// - Returns: A value of the requested type, if present and convertible to `Double`.
+    /// - Throws: `DecodingError.typeMismatch` if the encountered encoded value is not convertible to `Double`.
+    /// - Throws: `DecodingError.valueNotFound` if the encountered encoded value is null, or if there are no more values to decode.
+    ///- Author: Scott Brenner | SBFoundation
+    public mutating func decode<M>(_ type: M.Type = M.self, unit: M.UnitType) throws -> M
+    where M: MeasurementProtocol {
+        M(try decode(Double.self), unit)
+    }
+    
+    /// Decodes a value of the given type, if present.
+    ///
+    /// This method returns `nil` if the container has no elements left to decode, or if the value is null.
+    /// The difference between these states can be distinguished by checking `isAtEnd`.
+    /// - Parameter type: The type of value to decode.
+    /// - Parameter unit: The unit of measure.
+    /// - Returns: A decoded value of the requested type, or `nil` if the value is a null value, or if there are no more elements to decode.
+    /// - Throws: `DecodingError.typeMismatch` if the encountered encoded value is not convertible to `Double`.
+    ///- Author: Scott Brenner | SBFoundation
+    public mutating func decodeIfPresent<M>(_ type: M.Type = M.self, unit: M.UnitType) throws -> M?
+    where M: MeasurementProtocol {
+        M(try decodeIfPresent(Double.self), unit)
+    }
+}
+
 extension KeyedDecodingContainer {
     
     /// Decodes a value of the given type for the given key.
@@ -42,6 +71,9 @@ extension KeyedDecodingContainer {
     }
     
     /// Decodes a value of the given type for the given key, if present.
+    ///
+    /// This method returns `nil` if the container does not have a value associated with the key, or if the value is null.
+    /// The difference between these states can be distinguished with a `contains(_:)` call.
     /// - Parameter type: The type of value to decode.
     /// - Parameter key: The key that the decoded value is associated with.
     /// - Parameter unit: The unit of measure.
--- Tests/SBFoundationTests/Measurement/MeasurementProtocolTests.swift
+++ Tests/SBFoundationTests/Measurement/MeasurementProtocolTests.swift
@@ -28,9 +28,42 @@ final class MeasurementProtocolTests: XCTestCase {
         }
     }
     
-    func test() throws {
+    private struct Runway2: Decodable {
+        
+        struct Response: Codable {
+            
+            let length_ft: Double
+            
+            let displaced_threshold_ft: Double?
+            
+            func encode(to encoder: any Encoder) throws {
+                var container = encoder.unkeyedContainer()
+                try container.encode(length_ft)
+                try container.encode(displaced_threshold_ft)
+            }
+        }
+        
+        let length: Length
+        
+        let displacedThreshold: Length?
+        
+        init(from decoder: Decoder) throws {
+            var container = try decoder.unkeyedContainer()
+            length = try container.decode(unit: .feet)
+            displacedThreshold = try container.decodeIfPresent(unit: .feet)
+        }
+    }
+    
+    func testKeyedDecodingContainerDecoding() throws {
         let encodedData = try JSONEncoder().encode(Runway.Response(length_ft: 10000, displaced_threshold_ft: nil))
         let runway = try JSONDecoder().decode(Runway.self, from: encodedData)
+        XCTAssertEqual(runway.length, 10000(.feet))
+        XCTAssertNil(runway.displacedThreshold)
+    }
+    
+    func testUnkeyedDecodingContainerDecoding() throws {
+        let encodedData = try JSONEncoder().encode(Runway2.Response(length_ft: 10000, displaced_threshold_ft: nil))
+        let runway = try JSONDecoder().decode(Runway2.self, from: encodedData)
         XCTAssertEqual(runway.length, 10000(.feet))
         XCTAssertNil(runway.displacedThreshold)
     }
