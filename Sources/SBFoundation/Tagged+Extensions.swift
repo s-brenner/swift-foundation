59165615f81c5f8968ec2952ef76623a54353358
Scott Brenner <brenner.scott.980@icloud.com>
2025-11-15 13:03:45 -0700

    wip

--- Sources/SBFoundation/Tagged+Extensions.swift
+++ Sources/SBFoundation/Tagged+Extensions.swift
@@ -1,7 +1,15 @@
 #if canImport(Tagged) && (os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
 import Tagged
 
-public typealias TaggedID<T, RawValue> = Tagged<(T, id: ()), RawValue>
+public protocol InspectableTagProtocol {
+    associatedtype Root
+}
 
-public typealias TaggedUUID<T> = TaggedID<T, UUID>
+public enum IDTag<Root>: InspectableTagProtocol {
+    public typealias Value = (Root, id: ())
+}
+
+public typealias TaggedID<Root, RawValue> = Tagged<IDTag<Root>.Value, RawValue>
+
+public typealias TaggedUUID<Root> = TaggedID<Root, UUID>
 #endif
